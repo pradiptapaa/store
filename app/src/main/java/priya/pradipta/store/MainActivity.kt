270deb1d4dc99e76a25ffa92fda1914d0e626466
Pradipta Priya Adyatmika <pradipta.priya@salt.co.id>
2024-11-07 19:04:54 +0700

    feat: add profile

--- app/src/main/java/priya/pradipta/store/MainActivity.kt
+++ app/src/main/java/priya/pradipta/store/MainActivity.kt
@@ -5,18 +5,32 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
@@ -27,9 +41,11 @@ import org.koin.androidx.viewmodel.ext.android.viewModel
 import priya.pradipta.store.cart.CartModel
 import priya.pradipta.store.cart.item.BaseFilledIconButton
 import priya.pradipta.store.cart.list.CartViewModel
+import priya.pradipta.store.component.BaseImageRemote
 import priya.pradipta.store.product.detail.ProductDetailScreen
 import priya.pradipta.store.product.detail.ProductDetailUIState
 import priya.pradipta.store.product.detail.ProductDetailViewModel
+import priya.pradipta.store.product.list.BaseGap
 import priya.pradipta.store.product.list.ProductListModel
 import priya.pradipta.store.product.list.ProductListScreen
 import priya.pradipta.store.product.list.ProductListViewModel
@@ -40,6 +56,7 @@ import priya.pradipta.store.product.model.ProductModel
 import priya.pradipta.store.ui.theme.StoreTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -74,17 +91,57 @@ class MainActivity : ComponentActivity() {
                         }
                         composable<ProductListModel> {
                             val uiState by productListViewModel.uiState.collectAsState()
+                            var showBottomSheet by remember { mutableStateOf(false) }
+                            val sheetState = rememberModalBottomSheetState()
+
                             Box {
                                 ProductListScreen(
                                     productListUIState = uiState,
                                     onClick = { navController.navigate(it) },
                                     title = "Product List",
                                 )
-                                BaseFilledIconButton(
-                                    imageVector = Icons.Filled.ShoppingCart,
-                                    onClick = { navController.navigate(CartModel) },
-                                    modifier = Modifier.align(Alignment.TopEnd).padding(10.dp),
-                                )
+                                Row(modifier = Modifier.align(Alignment.TopEnd).padding(10.dp)) {
+                                    BaseFilledIconButton(
+                                        imageVector = Icons.Filled.ShoppingCart,
+                                        onClick = { navController.navigate(CartModel) },
+                                    )
+                                    BaseGap(8.dp)
+                                    BaseFilledIconButton(
+                                        imageVector = Icons.Filled.Person,
+                                        onClick = { showBottomSheet = true },
+                                    )
+                                }
+                            }
+
+                            if (showBottomSheet) {
+                                ModalBottomSheet(
+                                    onDismissRequest = {
+                                        showBottomSheet = false
+                                    },
+                                    sheetState = sheetState,
+                                ) {
+                                    Column(
+                                        modifier =
+                                            Modifier
+                                                .fillMaxSize()
+                                                .verticalScroll(rememberScrollState()),
+                                        horizontalAlignment = Alignment.CenterHorizontally,
+                                    ) {
+                                        BaseImageRemote(
+                                            url = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5k-7Hc--4L4A3-2GRrvZRY8HFOZZ71TpSZA&s",
+                                            modifier = Modifier.fillMaxWidth().aspectRatio(1.5f),
+                                        )
+                                        BaseGap(4.dp)
+                                        Text("Hello, I'm Pradipta Priya Adyamika")
+                                        BaseGap(4.dp)
+                                        Text(
+                                            "I don't have enough time to make this project because of work",
+                                            textAlign = TextAlign.Center,
+                                        )
+                                        BaseGap(4.dp)
+                                        Text("And I severely need a sleep.")
+                                    }
+                                }
                             }
                         }
                         composable<ProductModel> { backStackEntry ->
